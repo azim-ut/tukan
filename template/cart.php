68f45bf882431a336f5745c8354da08d5e8cbf5a
azim-ut <9420183@gmail.com>
2019-05-30 09:55:56 +0300

    empty pages

--- template/cart.php
+++ template/cart.php
@@ -1,64 +1,81 @@
 <? include_once __DIR__ . "/nav/start.php" ?>
-    <div class="row" ng-controller="CartListController">
-        <div class="col-md-1">
-            &nbsp;
+    <link type="text/css" rel="stylesheet" href="/web/css/cart_empty.css"/>
+
+    <div ng-controller="CartListController">
+        <div ng-if="!data.cart || !data.cart.length" style="text-align: center;">
+            <div class="emptyCart">
+                <div class="example">
+                    <b class="icon icon-basket"></b>
+                    Cart is empty
+                </div>
+            </div>
         </div>
+
+        <div class="row" ng-if="data.cart.length">
+            <div class="col-md-1">
+                &nbsp;
+            </div>
+
+
             <div class="col-md-7" style="
                                         overflow: hidden;
                                         padding: 0;
                                         border: #ccc 2px solid;">
-            <div class="row" ng-repeat="row in data.cart.items"
-                 style="margin: 0; border-bottom: #ccc 1px solid; background: #fff; padding: 10px;">
-                <div class="col-xs-2"
-                     style="width: 80px; height: 80px; border: #eaeaea 2px solid; background: #fff url('/wp-content/uploads/auto/{{row.img}}_200x200.jpg') no-repeat center center/cover">
-                    &nbsp;
-                </div>
-                <div class="col-xs-8">
-                    <table style="width: 100%;">
-                        <tr>
-                            <td rowspan="2">{{row.title}}</td>
-                            <td class="text-center" style="width: 1%;">
-                                <i class="icon-trash" style="font-weight: bolder;"></i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="text-center" style="width: 1%;">
-                                <i class="icon-heart" style="font-weight: bolder;"></i>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="col-xs-2 text-right">
-                    <div style="white-space: nowrap;">€ {{row.price}}</div>
+
+
+                <div class="row" ng-repeat="row in data.cart.items"
+                     style="margin: 0; border-bottom: #ccc 1px solid; background: #fff; padding: 10px;">
+                    <div class="col-xs-2"
+                         style="width: 80px; height: 80px; border: #eaeaea 2px solid; background: #fff url('/wp-content/uploads/auto/{{row.img}}_200x200.jpg') no-repeat center center/cover">
+                        &nbsp;
+                    </div>
+                    <div class="col-xs-8">
+                        <table style="width: 100%;">
+                            <tr>
+                                <td rowspan="2">{{row.title}}</td>
+                                <td class="text-center" style="width: 1%;">
+                                    <i class="icon-trash" style="font-weight: bolder;"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-center" style="width: 1%;">
+                                    <i class="icon-heart" style="font-weight: bolder;"></i>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="col-xs-2 text-right">
+                        <div style="white-space: nowrap;">€ {{row.price}}</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="row">
-                <div class="col-sm-12"
-                     style="overflow: hidden;
+            <div class="col-md-3">
+                <div class="row">
+                    <div class="col-sm-12"
+                         style="overflow: hidden;
                             background: #fff;
                             border: #ccc 2px solid;">
 
-                    <div class="text-right" style="line-height: 50px;">
-                        <span style="float: left;">Товары</span>
-                        <span class="text-right">x {{data.cart.items.length}}</span>
-                    </div>
+                        <div class="text-right" style="line-height: 50px;">
+                            <span style="float: left;">Товары</span>
+                            <span class="text-right">x {{data.cart.items.length}}</span>
+                        </div>
 
-                    <div style="line-height: 50px;">
-                        <div>Адрес доставки:</div>
-                        <p>{{data.cart.address}}</p>
-                    </div>
+                        <div style="line-height: 50px;">
+                            <div>Адрес доставки:</div>
+                            <p>{{data.cart.address}}</p>
+                        </div>
 
-                    <hr style="border-bottom: #ccc 1px solid; border-top: #333 1px solid; margin: 20px 0 0;"/>
-                    <div class="text-right" style="font-weight: bold; line-height: 50px; font-size: 130%;">
-                        <span style="float: left;">Итого</span>
-                        <span class="text-right">€ {{data.cart.sum}}</span>
+                        <hr style="border-bottom: #ccc 1px solid; border-top: #333 1px solid; margin: 20px 0 0;"/>
+                        <div class="text-right" style="font-weight: bold; line-height: 50px; font-size: 130%;">
+                            <span style="float: left;">Итого</span>
+                            <span class="text-right">€ {{data.cart.sum}}</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-1">
+            <div class="col-md-1">
+            </div>
         </div>
     </div>
 <? include_once __DIR__ . "/nav/footer.php" ?>
--- template/wishes.php
+++ template/wishes.php
@@ -1,26 +1,16 @@
 <? include_once __DIR__ . "/nav/start.php" ?>
+    <link type="text/css" rel="stylesheet" href="/web/css/wishlist_empty.css" />
 
     <div ng-controller="WishesListController">
         <div class="WishList text-center">
-            <div class="block info" style="position: relative;">
-                <div style="height: 90px;">
-                    <a href="tel:+37258185225" style="font-size: 100%;">
-                        <button class="btn btn-lg btn-icon-only btn-block">
-                            <i class="icon-call-out"></i>
-                        </button>
-                    </a>
-                </div>
-                <div style="height: 110px; font-size: 140%; font-weight: 700;" class="text-center">
-                    Закажите доставку сегодня.
-                </div>
-                <div>
-                    <a href="//www.facebook.com/groups/1152263238270402/">
-                        <button class="btn btn-circle btn-block btn-lg facebook-button">
-                            <i class="icon-social-facebook"></i>
-                        </button>
-                    </a>
+
+            <div class="emptyWish" ng-if="!data.wishes || !data.wishes.list.length">
+                <div class="example">
+                    <b class="icon icon-heart"></b>
+                    Wish list is empty
                 </div>
             </div>
+
 
             <div ng-repeat="row in data.wishes.list" class="col-sm-4">
                 <product-preview id="row.id"
