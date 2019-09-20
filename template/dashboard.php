e472e188f8cf71a39aff486376efde8741a2feaa
Aleksandr Kostiukov <akm@leongaming.com>
2019-09-20 19:01:28 +0300

    fix

---
+++ template/dashboard.php
@@ -0,0 +1,14 @@
+<? include_once __DIR__ . "/nav/start.php" ?>
+
+
+<?// include_once __DIR__ . "/nav/promo.php" ?>
+<?
+
+?>
+Dashboard
+
+
+
+
+
+<? include_once __DIR__ . "/nav/footer.php" ?>
---
+++ template/login/facebook.php
@@ -0,0 +1,5 @@
+<? include_once __DIR__ . "/../nav/start.php" ?>
+
+Dashboard
+
+<? include_once __DIR__ . "/../nav/footer.php" ?>
