e4b759e15afd2e122be40f30a0b79c73d6f77256
azim-ut <9420183@gmail.com>
2019-10-17 21:13:29 +0300

    fix

--- template/login/facebook.php
+++ template/login/facebook.php
@@ -1,5 +1,13 @@
-<? include_once __DIR__ . "/../nav/start_empty.php" ?>
+<? use core\service\FacebookAuthService;
 
-Facebook logged
+include_once __DIR__ . "/../nav/start_empty.php" ?>
+<?
+FacebookAuthService::getInstance()->log($_GET);
+?>
+<div class="row">
+    <div class="col-xs-2">&nbsp;</div>
+    <div class="col-xs-2">&nbsp;</div>
+    <div class="col-xs-2">&nbsp;</div>
+</div>
 
 <? include_once __DIR__ . "/../nav/footer_empty.php" ?>
