1d601f4ee8f12f130c92e0366c7c9be949829302
Aleksandr Kostiukov <akm@leongaming.com>
2019-10-21 17:15:23 +0300

    fix

--- template/login/facebook.php
+++ template/login/facebook.php
@@ -22,43 +22,35 @@ $expiresIn   = ParamsManager::getParam("expires_in");
 
 /** got code, but still has no access_token */
 if(!$user && $state === SessionManager::id() && $code != null){
-    $getTokenPath = FacebookConstants::changeCodeToAccessTokenPath($appID, $appSecret, $code, $redirectUrl);
-    $content      = file_get_contents($getTokenPath);
-    $res          = json_decode($content);
-    FacebookAuthService::getInstance()->log($res);
-    $accessToken = $res->access_token ?? null;
+	$getTokenPath = FacebookConstants::changeCodeToAccessTokenPath($appID, $appSecret, $code, $redirectUrl);
+	$content      = file_get_contents($getTokenPath);
+	$res          = json_decode($content);
+	FacebookAuthService::getInstance()->log($res);
+	$accessToken = $res->access_token ?? null;
 
-    /** got access_token and */
-    if($code != null && SessionManager::id() === $state && $appSecret != null){
-        $checkTokenPath = FacebookConstants::getCodeDebugPath($accessToken, $accessToken);
-        $content = file_get_contents($checkTokenPath);
-        $res = json_decode($content);
-        if(boolval($res->data->is_valid??false)){
-            $infoPath = FacebookConstants::getUserInfoPath($accessToken);
-            $content  = file_get_contents($infoPath);
-            $info     = json_decode($content);
-            $id    = $info->id ?? 0;
-            $name  = $info->name;
-            $email = $info->email ?? null;
-            try{
-                $user = UserManager::facebook($info->id, $email, $name);
-                FacebookAuthService::getInstance()->appendToSession(SessionManager::id(), $user->getId(), $token, $accessToken);
-                header("Location:/");
-            }catch(BadResultException $e){
-                var_dump($e);
-            }catch(NoUserException $e){
-                var_dump($e);
-            }
-	        exit();
-        }else{
-            echo "Fail";
-            exit();
-        }
-    }else{
-        echo "-";
-    }
-}else{
-    var_dump($user, $state, SessionManager::id(), $code);
+	/** got access_token and */
+	if($code != null && SessionManager::id() === $state && $appSecret != null){
+		$checkTokenPath = FacebookConstants::getCodeDebugPath($accessToken, $accessToken);
+		$content        = file_get_contents($checkTokenPath);
+		$res            = json_decode($content);
+		if(boolval($res->data->is_valid ?? false)){
+			$infoPath = FacebookConstants::getUserInfoPath($accessToken);
+			$content  = file_get_contents($infoPath);
+			$info     = json_decode($content);
+			$id       = $info->id ?? 0;
+			$name     = $info->name;
+			$email    = $info->email ?? null;
+			try{
+				$user = UserManager::facebook($info->id, $email, $name);
+				FacebookAuthService::getInstance()->appendToSession(SessionManager::id(), $user->getId(), $token, $accessToken);
+			}catch(BadResultException | NoUserException $e){
+				FacebookAuthService::getInstance()->log($e);
+			}
+			header("Location:/");
+		}
+	}
+	header("Location:/");
+	exit();
 }
 
 ?>
