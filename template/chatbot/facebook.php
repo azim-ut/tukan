fb513c2c652c9a0c11f88e515c27be567475dc31
Aleksandr Kostiukov <akm@leongaming.com>
2019-10-22 17:14:41 +0300

    changed fb marker

--- template/chatbot/facebook.php
+++ template/chatbot/facebook.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = "EAAGipka04bgBAIIHDEUprONwZCEPkRHwYiICxZAMTLCXfiN9dYMDh3dQ4kTMZBZC3PeJgiEKQL46P1n78tBtpKEz5ZB68ZBWy9bu5m9MiJvSw3BExRPFpQ0eRCKRsZCA0bj2Knsw5ZAh25GAkjZA7tPdjf3Limj2IrOA0NcO7Yp3TVT7hKWqqO0aF";
+$access_token = "EAAGipka04bgBALkxrZB8qhg3JLHfRq5D8wMtZCsj0XVxTfTZBr9YFxNfI19Ka0bTlVrA3dY6hUO91WuNpBpGY5OPKgIOCY4hfuccYuZAt5bcjXTvuouMWcD4ZC4ZCN6nzfnZCguRkeSoZCv1GJIbsv6lhqYEd6Lbga3AZCVsPoRQQ5fQmN6jZCKeD9";
 $verify_token = "fb_time_bot";
 $hub_verify_token = null;
 
