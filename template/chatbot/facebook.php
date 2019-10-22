19a6064f02372c85a0fccbb6b04447cc3c66d203
azim-ut <9420183@gmail.com>
2019-10-22 21:50:35 +0300

    added log to the chatbot listener

--- template/chatbot/facebook.php
+++ template/chatbot/facebook.php
@@ -1,12 +1,17 @@
 <?php
 
 use core\manager\ParamsManager;
+use core\service\FacebookChatService;
+use core\service\MySqlService;
 
 $access_token     = "EAAGipka04bgBALkxrZB8qhg3JLHfRq5D8wMtZCsj0XVxTfTZBr9YFxNfI19Ka0bTlVrA3dY6hUO91WuNpBpGY5OPKgIOCY4hfuccYuZAt5bcjXTvuouMWcD4ZC4ZCN6nzfnZCguRkeSoZCv1GJIbsv6lhqYEd6Lbga3AZCVsPoRQQ5fQmN6jZCKeD9";
 $verify_token     = "fb_time_bot";
 $hub_verify_token = null;
 $hubChallenge     = ParamsManager::getParam("hub_challenge");
 $hubVerifyToken   = ParamsManager::getParam("hub_verify_token");
+$sql              = MySqlService::getInstance();
+$params           = ParamsManager::paramsToString();
+FacebookChatService::getInstance()->log($params);
 if($hubChallenge){
     $challenge        = $hubChallenge;
     $hub_verify_token = $hubVerifyToken;
