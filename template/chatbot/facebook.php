b78fe615bd08172c7aa8e025b8e0899782c77d03
azim-ut <9420183@gmail.com>
2019-10-22 20:58:50 +0300

    updated chatbot back url

--- template/chatbot/facebook.php
+++ template/chatbot/facebook.php
@@ -1,14 +1,18 @@
 <?php
-$access_token = "EAAGipka04bgBALkxrZB8qhg3JLHfRq5D8wMtZCsj0XVxTfTZBr9YFxNfI19Ka0bTlVrA3dY6hUO91WuNpBpGY5OPKgIOCY4hfuccYuZAt5bcjXTvuouMWcD4ZC4ZCN6nzfnZCguRkeSoZCv1GJIbsv6lhqYEd6Lbga3AZCVsPoRQQ5fQmN6jZCKeD9";
-$verify_token = "fb_time_bot";
-$hub_verify_token = null;
 
-if(isset($_REQUEST['hub_challenge'])) {
-	$challenge = $_REQUEST['hub_challenge'];
-	$hub_verify_token = $_REQUEST['hub_verify_token'];
+use core\manager\ParamsManager;
+
+$access_token     = "EAAGipka04bgBALkxrZB8qhg3JLHfRq5D8wMtZCsj0XVxTfTZBr9YFxNfI19Ka0bTlVrA3dY6hUO91WuNpBpGY5OPKgIOCY4hfuccYuZAt5bcjXTvuouMWcD4ZC4ZCN6nzfnZCguRkeSoZCv1GJIbsv6lhqYEd6Lbga3AZCVsPoRQQ5fQmN6jZCKeD9";
+$verify_token     = "fb_time_bot";
+$hub_verify_token = null;
+$hubChallenge     = ParamsManager::getParam("hub_challenge");
+$hubVerifyToken   = ParamsManager::getParam("hub_verify_token");
+if($hubChallenge){
+    $challenge        = $hubChallenge;
+    $hub_verify_token = $hubVerifyToken;
 }
 
 
-if ($hub_verify_token === $verify_token) {
-	echo $challenge;
+if($hub_verify_token === $verify_token){
+    echo $challenge;
 }
