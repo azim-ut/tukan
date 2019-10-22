06af434a2b3921f514873a790c83ea959623c30f
azim-ut <9420183@gmail.com>
2019-10-22 23:09:44 +0300

    fix

--- template/chatbot/facebook.php
+++ template/chatbot/facebook.php
@@ -4,6 +4,7 @@ use core\manager\ParamsManager;
 use core\service\FacebookChatService;
 use core\service\MySqlService;
 use core\utils\ServerUtils;
+use core\utils\StringUtils;
 
 $accessToken      = "EAAGipka04bgBALkxrZB8qhg3JLHfRq5D8wMtZCsj0XVxTfTZBr9YFxNfI19Ka0bTlVrA3dY6hUO91WuNpBpGY5OPKgIOCY4hfuccYuZAt5bcjXTvuouMWcD4ZC4ZCN6nzfnZCguRkeSoZCv1GJIbsv6lhqYEd6Lbga3AZCVsPoRQQ5fQmN6jZCKeD9";
 $verify_token     = "fb_time_bot";
@@ -27,7 +28,7 @@ $input = json_decode(file_get_contents('php://input'));
 $recipientID      = ($input->entry[0]->messaging[0]->recipient->id) ?? - 1;
 $senderID         = ($input->entry[0]->messaging[0]->sender->id) ?? - 1;
 $messageInputText = ($input->entry[0]->messaging[0]->message->text) ?? "Hi";
-if(!$messageInputText){
+if(StringUtils::isEmpty($messageInputText)){
     exit();
 }
 FacebookChatService::getInstance()->log([$recipientID, $senderID, $messageInputText]);
