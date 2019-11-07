23d4cfdff558953dbaf0cc43891e68510ae56c11
azim-ut <9420183@gmail.com>
2019-11-07 23:03:18 +0200

    fix

--- template/filters/main_filters.php
+++ template/filters/main_filters.php
@@ -7,7 +7,7 @@ $height  = [70, 76, 82, 88, 93, 98, 104, 110, 116, 122, 128, 134, 140, 146, 152]
 $genders = [0 => 'Для мальчиков и девочек', 1 => 'Для мальчиков', 2 => 'Для девочек'];
 ?>
 <div style="text-align: center;">
-    <div class="row padding-top-15 padding-bottom-15" style="padding: 0;">
+    <div class="row padding-top-15 padding-bottom-15">
 
         <div class="col-sm-3 padding-bottom-5">
             <div class="input-group">
