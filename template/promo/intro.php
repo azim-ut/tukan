4969ae51b38a022b4a1d015415fb3a9eab0c716d
azim-ut <9420183@gmail.com>
2019-12-07 15:01:32 +0200

    fix

--- template/promo/intro.php
+++ template/promo/intro.php
@@ -3,12 +3,12 @@
         <div class="col-sm-6 text-center"
              onclick="location.href='/catalog?gender=2';"
              style="background: #ffffff url(/web/img/child_t20_kRv8Jx.jpg) no-repeat center top/cover; height:400px;">
-            <div class="ttl">Одежда для девочек<span>Italian brans</span></div>
+            <div class="ttl">Одежда для девочек<span>Italian brands</span></div>
         </div>
         <div class="col-sm-6 text-center"
              onclick="location.href='/catalog?gender=1';"
              style="background: #ffffff url(/web/img/happy-skater-kid_t20_3QAJd9.jpg) no-repeat center top/cover; height:400px;">
-            <div class="ttl">Одежда для мальчиков<span>Italian brans</span></div>
+            <div class="ttl">Одежда для мальчиков<span>Italian brands</span></div>
         </div>
     </div>
 </div>
