3fb14c95575f30f21c2db851c83052418ce7c4f6
azim-ut <9420183@gmail.com>
2019-12-07 19:53:51 +0200

    fix

--- template/promo/sales.php
+++ template/promo/sales.php
@@ -4,7 +4,7 @@
              onclick="location.href='/catalog?gender=2';"
              style="background: #ffffff url(/web/img/red_bg.jpg) no-repeat center top/cover; height:300px; color: #fff; position: relative;">
             <div style="display: inline-block; vertical-align: middle; position: relative; top: 30%;">
-                <div style="font-size: 300%; text-transform: uppercase;">Распродажа</div>
+                <div style="font-size: 270%; text-transform: uppercase;">Распродажа</div>
                 <div class="text-right"
                      style="font-size: 150%; color: #00ff00; font-weight: 500; line-height: 50%;"><?=date('d/m/Y')?></div>
             </div>
