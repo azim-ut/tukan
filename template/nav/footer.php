fb742a62d66d2cc57638ab20dfa5e6e4271c68a2
azim-ut <9420183@gmail.com>
2019-12-08 19:07:29 +0200

    added google analytics

--- template/nav/footer.php
+++ template/nav/footer.php
@@ -41,6 +41,16 @@
 <!-- END MAIN FOOTER -->
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-47064836-2"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129783874-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-129783874-1');
+</script>
+
 
 </body>
 </html>
