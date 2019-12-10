85de84ada44fd0b79f4b84388dc6c7094e9b0724
azim-ut <9420183@gmail.com>
2019-12-10 23:38:25 +0200

    updated cart row

--- template/promo/intro.php
+++ template/promo/intro.php
@@ -2,13 +2,24 @@
     <div class="row">
         <div class="col-sm-6 text-center"
              onclick="location.href='/catalog?gender=2';"
-             style="background: #ffffff url(/web/img/child_t20_kRv8Jx.jpg) no-repeat center top/cover; height:400px;">
-            <div class="ttl">Одежда для девочек<span>Italian brands</span></div>
+             style="background: #ffffff url(/web/img/child_t20_kRv8Jx.jpg) no-repeat center top/cover; height:400px; display:block;">
+
+            <button type="button"
+                    class="btn btn-success"
+                    style="margin-top: 320px;"
+                    onclick="location.href='/catalog?gender=2';">
+                Одежда для девочек
+            </button>
         </div>
         <div class="col-sm-6 text-center"
              onclick="location.href='/catalog?gender=1';"
              style="background: #ffffff url(/web/img/happy-skater-kid_t20_3QAJd9.jpg) no-repeat center top/cover; height:400px;">
-            <div class="ttl">Одежда для мальчиков<span>Italian brands</span></div>
+            <button type="button"
+                    class="btn btn-success"
+                    style="margin-top: 320px;"
+                    onclick="location.href='/catalog?gender=1';">
+                Одежда для мальчиков
+            </button>
         </div>
     </div>
 </div>
