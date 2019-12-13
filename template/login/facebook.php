20158e21951b53c928c8ea64b12eb47c55e6e602
azim-ut <9420183@gmail.com>
2019-12-13 23:21:57 +0200

    fix

--- template/login/facebook.php
+++ template/login/facebook.php
@@ -22,9 +22,10 @@ $token     = ParamsManager::getParam("token");
 $tokenType = ParamsManager::getParam("token_type");
 $expiresIn = ParamsManager::getParam("expires_in");
 
+$stateIsValid = (SessionManager::id() === $state || $state === 'lottery');
 
 /** got code, but still has no access_token */
-if(!$user && $state === SessionManager::id() && $code != null){
+if(!$user && $stateIsValid && $code != null){
 
 	$getTokenPath = FacebookConstants::changeCodeToAccessTokenPath($code);
 	$content      = file_get_contents($getTokenPath);
@@ -33,7 +34,7 @@ if(!$user && $state === SessionManager::id() && $code != null){
 	$accessToken = $res->access_token ?? null;
 
 	/** got access_token and */
-	if($code != null && (SessionManager::id() === $state || $state === 'lottery')){
+	if($code != null && $stateIsValid){
 		$app_token      = "EAAGipka04bgBAHASuftyl5pcMvT5r92qtBgCoZBRWenWslhAN4WyyQTKox8vqVKilcruoZCytCSn9BpbgJZBJSnPGrBz9CLFmcTS72hRq1vtvvgv9qNAOwzw5eOZA8b9z1QTNcbEM027dWS3ZBZAENSc8YypnMc4AespE5k7sZBcgZDZD";
 		$checkTokenPath = FacebookConstants::getCodeDebugPath($accessToken, $app_token);
 		$content        = file_get_contents($checkTokenPath);
