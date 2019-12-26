e9bb9416fea84b9babb6b8c20aa631d472f36776
azim-ut <9420183@gmail.com>
2019-12-26 18:06:49 +0200

    fix

--- template/nav/footer.php
+++ template/nav/footer.php
@@ -50,7 +50,16 @@
 
     gtag('config', 'UA-129783874-1');
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-155076505-1">
+</script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
+    gtag('config', 'UA-155076505-1');
+</script>
 
 </body>
 </html>
