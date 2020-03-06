bfe6997525cbe9229709b52ff27caed4413532ff
Aleksandr Kostiukov <akm@leongaming.com>
2020-03-06 14:31:44 +0200

    init

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ ini_set("log_errors" , "1");
 ini_set("error_log" , "errors.log");
 date_default_timezone_set("Europe/Tallinn");
 
+
 require_once(__DIR__ . "/dto/Properties.php");
 require_once(__DIR__ . "/dto/Catalog.php");
 require_once(__DIR__ . "/dto/Translate.php");
