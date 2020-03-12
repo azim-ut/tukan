023295698b84b3c342f28878e8df64f2380403c9
azim-ut <9420183@gmail.com>
2020-03-13 00:20:24 +0200

    fix

--- dto/Facebook.php
+++ dto/Facebook.php
@@ -27,7 +27,7 @@ class Facebook extends RemoteBase{
         $params[] = "token=".$token;
         $params[] = "tokenType=".$tokenType;
         $params[] = "expiresIn=".$expiresIn;
-        return self::src( "/core/rest/facebookAuth/login?".join("&", $params), "GET", true);
+        return self::src( "/core/rest/facebookAuth/login?".join("&", $params));
     }
 
     public function log($path, $content, $line){
