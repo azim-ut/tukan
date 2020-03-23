29ca04f1059f135c3cb5afd1804642d57874818d
Aleksandr Kostiukov <akm@leongaming.com>
2020-03-23 22:18:58 +0200

    exzoom replaced

--- assets/exzoom/exzoom.js
+++ assets/exzoom/exzoom.js
@@ -1,21 +1,19 @@
-$(function(){
-
-    $("#exzoom").exzoom({
-
-    // thumbnail nav options
-    "navWidth": 60,
-    "navHeight": 60,
-    "navItemNum": 5,
-    "navItemMargin": 7,
-    "navBorder": 1,
-
-    // autoplay
-    "autoPlay": false,   
-    });
+$(function () {
+    let options = {
+        navWidth: 60,
+        navHeight: 60,
+        navItemNum: 5,
+        navItemMargin: 7,
+        navBorder: 1,
+        autoPlay: false
+    };
+    let res = $("#exzoom").exzoom(options);
+    console.log($("#exzoom").exzoom);
 
 });
-function deleteexzoomdn(){
-        $("#exzoom-dn").removeAttr("style");
-        $("#exzoom-dn-nav").removeAttr("style");
-    };
-    setTimeout(deleteexzoomdn,300);                 
+
+function deleteexzoomdn() {
+    $("#exzoom-dn").removeAttr("style");
+    $("#exzoom-dn-nav").removeAttr("style");
+};
+setTimeout(deleteexzoomdn, 300);
